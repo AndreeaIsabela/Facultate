fe6236662a1518e7550f7e2882d3746af675f9a9
Isabela Andreea <bala.andreea@yahoo.com>
2018-05-18 01:52:08 +0300

    cuda changes

--- PDA/Rand/Rand/Program.cs
+++ PDA/Rand/Rand/Program.cs
@@ -20,37 +20,36 @@ namespace Rand
                 var com = Intercommunicator.world;
                 var randomGen=new Random(System.DateTime.Now.Millisecond+com.Rank);
                 int no = randomGen.Next(100, 1000); ;
-                com.Barrier();
-                List<int> arr= com.Gather(no, 0).ToList<int>();
-                //if(com.Rank!=0)
-                //{
 
-                //    com.Send(no, 0,com.Rank);
+                if (com.Rank != 0)
+                {
+
+                    com.Send(no, 0, com.Rank);
 
 
-                //    Console.WriteLine("Procesul "+com.Rank+" a generat :"+ no );
+                    Console.WriteLine("Procesul " + com.Rank + " a generat :" + no);
 
-                //}
-                //
-                if(com.Rank==0)
+                }
+
+                if (com.Rank==0)
                 {
                     int maxNo = no, rank =0;
-                    //int recVal = 0;
-                    //Console.WriteLine("Procesul " + com.Rank +" a generat  :"  + no);
-                    //for (int i = 1; i < procNr; ++i)
-                    //{
-                    //    recVal = com.Receive<int>(i, i);
+                    int recVal = 0;
+                    Console.WriteLine("Procesul " + com.Rank +" a generat  :"  + no);
+                    for (int i = 1; i < procNr; ++i)
+                    {
+                        recVal = com.Receive<int>(i, i);
 
-                    //    if (maxNo < recVal)
-                    //    {
-                    //        maxNo = recVal;
-                    //        rank = i;
-                    //    }
-                    //    else if(maxNo == recVal)
-                    //    {
-                    //        rank = Math.Max(rank,i);
+                        if (maxNo < recVal)
+                        {
+                            maxNo = recVal;
+                            rank = i;
+                        }
+                        else if(maxNo == recVal)
+                        {
+                            rank = Math.Max(rank,i);
 
-                    //    }
+                        }
 
 
 
